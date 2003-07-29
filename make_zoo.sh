6e38bf9ad75706cfd1b563b57a1048bc079c441d
bh11 <bh11@users.noreply.github.com>
2003-07-29 14:49:21 +0000

    fixed some bugs in make_zoo.sh. Burkhard.

--- make_zoo.sh
+++ make_zoo.sh
@@ -1,4 +1,4 @@
-#! /bin/csh
+#!/bin/csh
 cd ../
 rm irredsol.zoo
 
@@ -11,42 +11,46 @@ set docfiles = (manual.tex overview.tex access.tex matgroups.tex \
     
 set manfiles = (.bbl .ind .idx .six .dvi .pdf)
 
-set testfiles = ()
+set testfiles = (test.tst testload.g)
 
 zoo ach irredsol irredsol/PackageInfo.g <irredsol/text_comment
 zoo ach irredsol irredsol/init.g <irredsol/text_comment
 zoo ach irredsol irredsol/read.g <irredsol/text_comment
 
-for file ($libfiles)
+foreach file ($libfiles)
    zoo ach irredsol irredsol/lib/$file <irredsol/text_comment
 end
 
-for file ($docfiles)
+foreach file ($docfiles)
    zoo ach irredsol irredsol/doc/$file <irredsol/text_comment
 end
 
-for file ($manfiles)
+foreach file ($manfiles)
    zoo ach irredsol irredsol/doc/manual$file <irredsol/text_comment
 end
 
-for file ($testfiles)
+foreach file ($testfiles)
    zoo ach irredsol irredsol/tst/$file <irredsol/text_comment
 end
 
-for file (/htm/*)
-	zoo ach irredsol irredsol/$file <irredsol/text_comment
+foreach file (irredsol/htm/*)
+	zoo ach irredsol $file <irredsol/text_comment
 end
 
-for file (/data/*)
-	zoo ach irredsol irredsol/$file <irredsol/text_comment
+foreach file (irredsol/data/*)
+	zoo ach irredsol $file <irredsol/text_comment
 end
 
-for file (/fps/*.idx)
-	zoo ach irredsol irredsol/$file <irredsol/text_comment
+foreach file (irredsol/fps/*.idx)
+	zoo ach irredsol $file <irredsol/text_comment
 end
 
-for file (/fps/*.fpc)
-	zoo ach irredsol irredsol/$file <irredsol/text_comment
+foreach file (irredsol/fps/*.fpc)
+	zoo ach irredsol $file <irredsol/text_comment
 end
 
 zoo ach irredsol irredsol/README <irredsol/text_comment
+
+
+
+
