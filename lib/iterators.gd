d59709bff35cb3f62df0bca84c70ee22d7d36948
bh11 <bh11@users.noreply.github.com>
2005-02-14 12:19:40 +0000

    Cleaned up global variables, introduced minimal block dimension als selection criterion, various minor improvements. Burkhard.

--- lib/iterators.gd
+++ lib/iterators.gd
@@ -1,26 +1,27 @@
 ############################################################################
 ##
-##  iterators.gd                 IRREDSOL                 Burkhard H\"ofling
+##  iterators.gd                 IRREDSOL                 Burkhard Hoefling
 ##
 ##  @(#)$Id$
 ##
-##  Copyright (C) 2003 by Burkhard H\"ofling, 
-##  Institut f\"ur Geometrie, Algebra und Diskrete Mathematik
-##  Technische Universit\"at Braunschweig, Germany
+##  Copyright (C) 2003-2005 by Burkhard Hoefling, 
+##  Institut fuer Geometrie, Algebra und Diskrete Mathematik
+##  Technische Universitaet Braunschweig, Germany
 ##
 
 
 ############################################################################
 ##
-#F  SelectionAbsolutelyIrreducibleSolvableMatrixGroups(n, q, orders, blockdims, max)
+#F  SelectionIrreducibleSolvableMatrixGroups(n, q, d, indices, orders, blockdims, max)
 ##
-##  selects the set of indices of absolutely irreducible matrix gropus
-##  whose orders are in <orders>, whose minimal block dims are in <blockdims>
-##  if max is true, only the maximal soluble groups are returned, if max is
-##  false, the non-maximal ones are returned. 
+##  selects the subset of <indices> corresponding to those irreducible 
+##  matrix gropus whose orders are in <orders>, whose minimal block dims are in 
+##  <blockdims>. if max is true and d = 1, only the maximal solvable groups are returned,
+##  if max is false, the non-maximal ones are returned. 
 ##  To ignore one of the parameters orders, blockdims, max, set it to fail
+##  If <indices> is fail, all groups are considered.
 ##  
-DeclareGlobalFunction ("SelectionAbsolutelyIrreducibleSolvableMatrixGroups");
+DeclareGlobalFunction ("SelectionIrreducibleSolvableMatrixGroups");
 
 
 ############################################################################
@@ -30,15 +31,6 @@ DeclareGlobalFunction ("SelectionAbsolutelyIrreducibleSolvableMatrixGroups");
 ##  see the IRREDSOL manual
 ##  
 DeclareGlobalFunction ("OrdersAbsolutelyIrreducibleSolvableMatrixGroups");
-
-
-############################################################################
-##
-#F  MaximalAbsolutelyIrreducibleSolvableMatrixGroup(<n>, <q>, <k>)
-##
-##  see the IRREDSOL manual
-##  
-DeclareGlobalFunction ("BlockDimensionsAbsolutelyIrreducibleSolvableMatrixGroups");
 
 
 ############################################################################
