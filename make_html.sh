9ce35bdfc5d44d8d3157ab2861b865f8a02610de
gap <devel@gap-system.org>
2005-07-06 09:49:50 +0000

    changed to rm -f to suppress error messages

--- make_html.sh
+++ make_html.sh
@@ -1,6 +1,6 @@
-rm htm/CHAP???.htm
-rm htm/biblio.htm
-rm htm/theindex.htm
-rm htm/chapters.htm
+rm -f htm/CHAP???.htm
+rm -f htm/biblio.htm
+rm -f htm/theindex.htm
+rm -f htm/chapters.htm
 perl ../../etc/convert.pl -n IRREDSOL -c -i doc htm
 chmod -R a+r htm
