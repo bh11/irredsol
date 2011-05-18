62c4ab2c8c35ba4dc8d33d1da3c4b08874e56c6c
bh11 <bh11@users.noreply.github.com>
2011-05-18 16:41:11 +0000

    move IdPrimitiveSolvableGroup into recognizeprim.gd. BH

--- lib/primitive.gd
+++ lib/primitive.gd
@@ -147,18 +147,6 @@ DECLARE_IRREDSOL_FUNCTION ("OnePrimitiveSolvablePermGroup");
 
 
 
-###########################################################################
-##
-#A  IdPrimitiveSolvableGroup(<grp>)
-#F  IdPrimitiveSolvableGroupNC(<grp>)
-##
-##  see IRREDSOL documentation
-##  
-DeclareAttribute ("IdPrimitiveSolvableGroup", IsGroup);
-DECLARE_IRREDSOL_SYNONYMS ("IdPrimitiveSolvableGroup");
-DECLARE_IRREDSOL_FUNCTION ("IdPrimitiveSolvableGroupNC");
-
-
 ############################################################################
 ##
 #E
